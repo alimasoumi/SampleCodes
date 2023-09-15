ae4b4e269c74aa7baa63f8c6ef9e33df9d8036e1
Ali <alimasumi@gmail.com>
2023-09-15 11:20:28 -0700

    1- range base query class added. 2- initial commit for the deep neural network class in python.

---
+++ FeedForwardNNClass/nueralNetwork.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class Layer:
+     def __init__(n_in, n_out, activation):
+        self.activation = activation 
+        self.weights = np.random.normal(shape =(n_in, n_out))
+
+
+class NeuralNetwork():
+    def self.__init__():
+        pass
+    
+
+
+x = Layer(2, 3, np.sin)
